e53fdacad17782e29e3cea860aee83cffbf8b118
Reto Kaiser <reto@cargomedia.ch>
2013-04-25 09:27:09 +0200

    Remove duplicate url

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -2,7 +2,6 @@ require 'formula'
 
 class Ghi < Formula
 	homepage 'https://github.com/stephencelis/ghi'
-	url 'https://github.com/stephencelis/ghi/archive/0.9.0.20121114.tar.gz'
 	url 'https://github.com/stephencelis/ghi/archive/0.9.0.20130424.tar.gz'
 	version '0.9.0.20130424'
 	sha1 '00c402ab1336667a8b7aefff6761e11273d07ed5'
